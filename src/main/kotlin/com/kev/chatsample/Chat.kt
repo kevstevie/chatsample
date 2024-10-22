3969ef181f8808114b56f44374627b0c5987a41c
kevstevie <jianna442@gmail.com>
2024-10-22 14:44:31 +0900

    feat: chat 초기 설계

--- src/main/kotlin/com/kev/chatsample/Chat.kt
+++ src/main/kotlin/com/kev/chatsample/Chat.kt
@@ -1,7 +1,14 @@
 package com.kev.chatsample
 
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
 class Chat(
+
+    @Id
+    val id: String = LocalDateTime.now().toString(),
     val senderId: Long,
     val content: String,
 ) {
+
 }
---
+++ src/main/kotlin/com/kev/chatsample/MongoConfig.kt
@@ -0,0 +1,16 @@
+package com.kev.chatsample
+
+import com.mongodb.client.MongoClients
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.MongoTemplate
+
+@Configuration
+class MongoConfig {
+
+    @Bean
+    fun mongoTemplate() = MongoTemplate(mongoClient(), "test")
+
+    @Bean
+    fun mongoClient() = MongoClients.create("mongodb://localhost:27017/test")
+}
